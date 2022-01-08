c9341d884c1d2106f43b883a08feef55bbac91a9
harelpanker <harelpanker@gmail.com>
2022-01-08 20:53:45 +0100

    fix

--- components/home/Header.js
+++ components/home/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className='min-h-[25vh] md:min-h-[30vh] px-4 flex flex-col justify-end'>
       <div className='container max-w-sm sm:max-w-screen-xl border-b border-gray-900 border-opacity-30 pb-4'>
-        <h1 className='font-bold text-5xl md:text-8xl'>Miri's Blog</h1>
+        <h1 className='font-bold text-5xl md:text-8xl'>Miri&apos;s Blog</h1>
       </div>
     </header>
   );
